d0b10d864bfe281e6c49dc7355b984270f7529a7
wonuk(du_ck) <wonuk007@naver.com>
2023-11-17 19:07:35 +0900

    aop 적용

--- hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -18,7 +18,7 @@ public class TimeTraceAop {
      * AOP 는 메서드의 호출시간을 측정하기 위해서 사용
      * 공통 시간측정 로직을 만든 뒤 측정하고싶은곳을 선언만 해주면 측정이 됨
      */
-    //@Around 가 측정하고 싶은곳을 타겟하는 어노테이션
+    //@Around 가 측정하고 싶은곳을 타겟하는 어노테이션s
     @Around("execution(* hello.hellospring..*(..))")  // >> 옆에 선언은 해당 패키지 아래 모두 적용
     public Object execute(ProceedingJoinPoint joinPoint)  throws Throwable {
         long start = System.currentTimeMillis();
